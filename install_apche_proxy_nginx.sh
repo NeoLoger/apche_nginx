b04686fcb185c98feaf24982accfe238eb070fb8
Yvggeniy Romanskiy <romanskiy.yvg1@gmail.com>
2020-10-27 22:47:31 +0200

    Update install_apche_proxy_nginx.sh

--- install_apche_proxy_nginx.sh
+++ install_apche_proxy_nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This scrip will install and configure NGINX as proxy server for Apache with Engintron caching and optimizations.
-# For more information see the official Engintron github https://github.com/engintron/engintron
+# For more information about the caching and NGINX optimizations see the official Engintron github https://github.com/engintron/engintron
 #
 
 # Stop Script if encontering an error.
@@ -9,6 +9,32 @@ set -e
 
 fqdn=null
 phpv=7.4
+
+enable_nginx(){
+	echo -e "\e[39mChanging Apache prots to 8080|8443"
+	sed -i 's/80/8080/g' /etc/apache2/ports.conf
+	sed -i 's/443/8443/g' /etc/apache2/ports.conf
+	sed -i 's/80/8080/g'  /etc/apache2/sites-available/*.conf
+	sed -i 's/443/8443/g'  /etc/apache2/sites-available/*.conf
+	systemctl restart apache2.service
+	echo "Starting NGINX"
+	systemctl enable nginx.service
+	systemctl start nginx.service
+	echo 1 > /etc/Engintron
+}
+
+disable_nginx(){
+	echo -e "\e[39mChanging Apache prots to 80|443"
+	sed -i 's/8080/80/g' /etc/apache2/ports.conf
+	sed -i 's/8443/443/g' /etc/apache2/ports.conf
+	sed -i 's/8080/80/g'  /etc/apache2/sites-available/*.conf
+	sed -i 's/8443/443/g'  /etc/apache2/sites-available/*.conf
+	echo "Stopping NGINX"
+	systemctl disable nginx.service
+	systemctl stop nginx.service
+	systemctl restart apache2.service
+	echo 0 > /etc/Engintron
+}
 
 self_signed_ssl(){
    openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "/C=NY/ST=NY/L=New Yourk/O=Yvggeniy/CN=$fqdn" -keyout /etc/ssl/private/$fqdn.key -out /etc/ssl/private/$fqdn.crt
@@ -27,9 +53,42 @@ install_phpfpm(){
 	add-apt-repository ppa:ondrej/php -y
 	apt update
 	
-	apt install libapache2-mod-php$phpv php$phpv-cli php$phpv-mysql php$phpv-gd php$phpv-imagick php$phpv-tidy php$phpv-xml php$phpv-xmlrpc php$phpv-curl php$phpv-mbstring php$phpv-bcmath php$phpv-soap php$phpv-zip php$phpv-intl -y  
+	apt install php$phpv-fpm libapache2-mod-php$phpv php$phpv-cli php$phpv-mysql php$phpv-gd php$phpv-imagick php$phpv-tidy php$phpv-xml php$phpv-xmlrpc php$phpv-curl php$phpv-mbstring php$phpv-bcmath php$phpv-soap php$phpv-zip php$phpv-intl -y  
 	# set pm settings
+	echo "Configuring www.conf file..."
+	sed -i "s/pm = dynamic/pm = ondemand/g" /etc/php/$phpv/fpm/pool.d/www.conf
+	sed -i "s/;pm.process_idle_timeout = 10s;/pm.process_idle_timeout = 30s;/g" /etc/php/$phpv/fpm/pool.d/www.conf
+	if free -m | awk '{ print $2}' | awk 'NR==2{print}' > 2000; then
+		echo "Server have 2GB RAM or less, stting pm.max_children to 40 "
+		sed -i "s/pm.max_children = 5/pm.max_children = 40/g" /etc/php/$phpv/fpm/pool.d/www.conf
+	elif  free -m | awk '{ print $2}' | awk 'NR==2{print}' > 4000 ; then
+		echo "Server have 4GB RAM or less, stting pm.max_children to 60 "
+		sed -i "s/pm.max_children = 5/pm.max_children = 60/g" /etc/php/$phpv/fpm/pool.d/www.conf
+	elif  free -m | awk '{ print $2}' | awk 'NR==2{print}' > 8000 ; then
+		echo "Server have 8GB RAM or less, stting pm.max_children to 80 "
+		sed -i "s/pm.max_children = 5/pm.max_children = 80/g" /etc/php/$phpv/fpm/pool.d/www.conf
+	else
+		echo "Server have more tahn 8GB of RAM, stting pm.max_children to 100"
+		sed -i "s/pm.max_children = 5/pm.max_children = 100/g" /etc/php/$phpv/fpm/pool.d/www.conf
+	fi
+
+	# set php.ini settings
+	echo "Configuring php.ini file..."
+	var=$(grep memory_limit /etc/php/$phpv/fpm/php.ini)
+	sed -i "s/$var/memory_limit = 256M/g" /etc/php/$phpv/fpm/php.ini
 	
+	var=$(grep post_max_size /etc/php/$phpv/fpm/php.ini)
+	sed -i "s/$var/post_max_size = 16M/g" /etc/php/$phpv/fpm/php.ini
+	
+	var=$(grep upload_max_filesize /etc/php/$phpv/fpm/php.ini)
+	sed -i "s/$var/upload_max_filesize = 64/g" /etc/php/$phpv/fpm/php.ini
+	
+	echo "Disabling high vulnerability php functions..."
+	var=$(grep disable_functions /etc/php/$phpv/fpm/php.ini)
+	sed -i "s/$var/disable_functions = ini_set,php_uname,getmyuid,getmypid,passthru,leak,listen,diskfreespace,tmpfile,link,ignore_user_abord,shell_exec,dl,set_time_limit,exec,system,highlight_file,source,show_source,fpaththru,virtual,posix_ctermid,posix_getcwd,posix_getegid,posix_geteuid,posix_getgid,posix_getgrgid,posix_getgrnam,posix_getgroups,posix_getlogin,posix_getpgid,posix_getpgrp,posix_getpid,posix,_getppid,posix_getpwnam,posix_getpwuid,posix_getrlimit,posix_getsid,posix_getuid,posix_isatty,posix_kill,posix_mkfifo,posix_setegid,posix_seteuid,posix_setgid,posix_setpgid,posix_setsid,posix_setuid,posix_times,posix_ttyname,posix_uname,proc_open,proc_close,proc_get_status,proc_nice,proc_terminate,popen,curl_exec,curl_multi_exec,parse_ini_file,allow_url_fopen,allow_url_include,pcntl_exec,chgrp,chmod,chown,lchgrp,lchown,putenv,pcntl_alarm,pcntl_fork,pcntl_waitpid,pcntl_wait,pcntl_wifexited,pcntl_wifstopped,pcntl_wifsignaled,pcntl_wifcontinued,pcntl_wexitstatus,pcntl_wtermsig,pcntl_wstopsig,pcntl_signal,pcntl_signal_get_handler,pcntl_signal_dispatch,pcntl_get_last_error,pcntl_strerror,pcntl_sigprocmask,pcntl_sigwaitinfo,pcntl_sigtimedwait,pcntl_exec,pcntl_getpriority,pcntl_setpriority,pcntl_async_signals,pcntl_unshare/g" /etc/php/$phpv/fpm/php.ini
+	
+	echo "Restarting php-fpm"
+	service php$phpv-fpm restart
 }
 
 install_apache(){
@@ -104,7 +163,7 @@ install_apache(){
 '> /etc/nginx/conf.d/default.conf
 	
 	a2ensite $fqdn
-	install_phpfpm $php
+	systemctl stop apache2.service
 	
 }
 
@@ -245,51 +304,99 @@ service nginx restart
 if netstat -ntlp | grep "80" | grep "nginx" && netstat -ntlp | grep "443" | grep "nginx" ; then
    echo -e "\e[32mNGINX Is UP on ports 80 and 443\e[39m"
 else
-   echo -e "\e[41mSomting went wrong wit NGINX configuration\e[39m"
+   echo -e "\e[41mSomting went wrong with NGINX configuration\e[39m"
 fi
 
 if netstat -ntlp | grep "80" | grep "apache" && netstat -ntlp | grep "443" | grep "apache" ; then
    echo -e "\e[32mApache Is UP on ports 8080 and 8443\e[39m"
    echo -e "\e[39mInstallation completed successfully!"
+   touch /etc/Engintron
+   echo 1 > /etc/Engintron
    
 else
-   echo -e "\e[41mSomting went wrong wit Apache configuration\e[39m"
+   echo -e "\e[41mSomting went wrong with Apache configuration\e[39m"
 fi
+
 }
 
 # Test if the server meets minimum requirements
-	# is apache installed?
-if [ ! -d /etc/apache2 ] ; then
-   echo -e "\e[31mApache is not installed on this server."
-   echo -e "Are you sure uknow waht you are doing?\e[39m"
-   echo -e "\e[39mPlease consult with an expert before proceeding."
-   exit 1
-else
-	# Test if NGINX is allredy installed on the server
-	if [ -d /etc/nginx ] ; then
-		echo -e "\e[31mNGINX allrdy installed..."
-		echo -e "Are you sure uknow waht you are doing?\e[39m"
+
+if [ -f /etc/Engintron ]; then
+	if	cat /etc/Engintron == 1; then
+		read -p "Disable NGINX Proxy?" -n 1 -r
+		echo ""
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			disable_nginx
+		fi
+	elif cat /etc/Engintron == 0; then
+		read -p "Enable NGINX Proxy?" -n 1 -r
+		echo ""
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			enable_nginx
+		fi
+	fi
+	
+else	
+		# is apache installed?
+	if [ ! -d /etc/apache2 ] && [ ! -f /etc/Engintron ] ; then
+		echo -e "\e[31mApache is not installed on this server."
+		echo -e "You are about to install Apache with NGINX as a proxy pleas make sure you are duing it on a fresh Ubuntu 18.04 server installation.\e[39m"
 		echo -e "\e[39mPlease consult with an expert before proceeding."
-		exit 1
+
+		read -p "Are you sure you want to install Apache and NGINX on this server? [y/n] " -n 1 -r
+		echo ""
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			echo "Starting Installation process... "
+			read -p "Please indicate PHP version (i.e: 7.4 or 7.3 and so on... ): " phpv
+			if [[ "$phpv" =~ ^[5-9]\.[0-9]$ ]]; then
+				echo "php$phpv Will be Installed."
+				read -p "Please imput FQDN (i.e: example.com ): " fqdn
+				apt update
+				install_apache
+				install_phpfpm
+				apt install nginx -y
+				install_engintron
+				
+			else
+				echo -e "\e[31m php$phpv \e[39mIs not a valid PHP version."
+				echo "Exiting.."
+			fi
+			
+		elif [[ $REPLY =~ ^[Nn]$ ]]; then
+			echo "Exiting."
+			exit 1
+		fi
+
+	
 	else
-		# Imput Domain name
-		echo "Please imput FQDN (i.e: example.com ):" 
-		read fqdn
+	# Test if NGINX is allredy installed on the server
+		if [ -d /etc/nginx ] ; then
+			echo -e "\e[31mNGINX allrdy installed..."
+			echo -e "Are you sure uknow waht you are doing?\e[39m"
+			echo -e "\e[39mPlease consult with an expert before proceeding."
+			exit 1
+		else
+			# Imput Domain name
+			read -p "Please imput FQDN (i.e: example.com ): " fqdn
 
    
-		# Stop Apache service
-		echo "Stoping Apache service..."
-		systemctl stop apache2.service
+			# Stop Apache service
+			echo "Stoping Apache service..."
+			systemctl stop apache2.service
    
-		# Install NGINX
-		echo "Instaling nginx..."
-		apt update
-		apt install nginx -y
-		systemctl enable nginx.service
-		install_engintron
-		
+			# Install NGINX
+			echo "Instaling nginx..."
+			apt update
+			apt install nginx -y
+			systemctl enable nginx.service
+			install_engintron
+		fi
 	fi
+
+
+	
 fi
+
 
 
 # End Scrip
